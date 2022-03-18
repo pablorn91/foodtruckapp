f982f436a82eb4d967bbcf25af33ad3047bfafa8
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-03-18 14:57:53 -0400

    calcular total a pagar

--- context/FoodtruckProvider.jsx
+++ context/FoodtruckProvider.jsx
@@ -11,6 +11,8 @@ const FoodtruckProvider = ({children}) => {
     const [ producto, setProducto ] = useState({})
     const [ modal, setModal ] = useState(false)
     const [ pedido, setPedido ] = useState([])
+    const [ nombre, setNombre ] = useState('')
+    const [ total, setTotal ] = useState(0)
 
     const router = useRouter()
 
@@ -26,6 +28,11 @@ const FoodtruckProvider = ({children}) => {
     useEffect(() => {
         setCategoriaActual(categorias[1])
     }, [categorias])
+
+    useEffect(() => {
+        const nuevoTotal = pedido.reduce((total,producto) => (producto.precio * producto.cantidad) + total, 0)
+        setTotal(nuevoTotal)
+    }, [pedido])
 
     const handleClickCategoria = id => {
         const categoria = categorias.filter( cat => cat.id === id)
@@ -69,6 +76,12 @@ const FoodtruckProvider = ({children}) => {
         setPedido(pedidoActualizado)
     }
 
+    const colocarOrden = async e => {
+        e.preventDefault()
+        console.log('enviando orden...')
+    }
+
+
     return (
 
         <FoodtruckContext.Provider
@@ -83,7 +96,11 @@ const FoodtruckProvider = ({children}) => {
                 handleAgregarPedido,
                 pedido,
                 handleEditarCantidades,
-                handleEliminarProducto
+                handleEliminarProducto,
+                nombre,
+                setNombre,
+                colocarOrden,
+                total
             }}
         >
             {children}
--- pages/total.js
+++ pages/total.js
@@ -1,6 +1,20 @@
+import { useEffect, useCallback } from "react"
 import Layout from "../layout/Layout"
+import useFoodtruck from "../hooks/useFoodtruck"
+import { formatearDinero } from '../helpers'
 
 export default function Total() {
+
+    const { pedido, nombre, setNombre, colocarOrden, total } = useFoodtruck()
+
+    
+    const comprobarPedido = useCallback ( () => {
+        return pedido.length === 0 || nombre === '' || nombre.length < 3
+    }, [pedido, nombre])
+
+    useEffect(() => {
+        comprobarPedido()
+    }, [pedido, comprobarPedido])
 
     return (
         <Layout 
@@ -8,6 +22,36 @@ export default function Total() {
         >
         <h1 className="text-4xl font-black">Total y Confirmar Pedido</h1>
         <p className="text-2xl my-10"> Confirma tu Pedido a Continuación</p>
+
+        <form
+            onSubmit={colocarOrden}
+        >
+            <div>
+                <label
+                    htmlFor="nombre"
+                    className="block uppercase text-slate-800 font-bold text-xl"
+                >Nombre</label>
+                <input
+                    id="nombre"
+                    type="text"
+                    className="bg-gray-200 w-full lg:w-1/3 p-2 mt-3 rounded"
+                    value={nombre}
+                    onChange={e => setNombre(e.target.value)}
+                />
+            </div>
+
+            <div className="mt-10">
+                <p className="text-2xl">Total a pagar {''} <span className="font-bold">{formatearDinero(total)}</span></p>
+            </div>
+            <div className="mt-5">
+                <input
+                    type="submit"
+                    className={`${comprobarPedido()  ? 'bg-indigo-100' : 'bg-indigo-600 hover:bg-indigo-800' } w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center`}
+                    value="Confirmar Pedido"
+                    disabled={comprobarPedido()}
+                />
+            </div>
+        </form>
         </Layout>
     )
 }
